f063f9d1bac097151b891ece480d66d450c92041
Kevin Nguyen <kevinnguyen@Kevins-MacBook-Air.local>
2021-10-02 23:17:30 -0700

    the code

---
+++ smallProgram.java
@@ -0,0 +1,16 @@
+package javaStuff;
+
+public class smallProgram {
+    public static void main(String[] args) {
+        int $s = 5;
+        System.out.println($s);
+        // int 5 = 5;  //error because can't start with number
+        int uninitVar;
+        // System.out.println(uninitVar);  //cant be used because not initialized
+        if(true) {
+            int x = 10;
+            System.out.printf("%i is a local variable in the if-block", x);
+        }
+        // System.out.println(x);   //x can't be resolved because it is local to the if-block
+    }  
+}
